5bacdb385088df6248f0ea058ba620fe22edc0c8
DKL <j210021@yahoo.com.tw>
2017-11-22 12:56:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/auser/myapplication/CustomToastQuest.java
@@ -0,0 +1,66 @@
+package com.example.android.customtoastquest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class CustomToastQuest extends Activity {
+
+    EditText et1,et2,et3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customtoastquest);
+        findView();
+        setOnKeyListener();
+    }
+
+    void findView(){
+        et1 = (EditText)findViewById(R.id.editText1);
+        et2 = (EditText)findViewById(R.id.editText2);
+        et3 = (EditText)findViewById(R.id.editText3);
+    }
+
+    void setOnKeyListener(){
+        et1.setOnKeyListener(onKeyListener);
+        et2.setOnKeyListener(onKeyListener);
+        et3.setOnKeyListener(onKeyListener);
+    }
+
+    OnKeyListener onKeyListener = new OnKeyListener(){
+
+        @Override
+        public boolean onKey(View v, int keyCode, KeyEvent event)
+        {
+            //判斷使用者是否有按下的動作以及按下的是否為Enter鍵
+            if(event.getAction()==KeyEvent.ACTION_DOWN & keyCode == KeyEvent.KEYCODE_ENTER){
+
+                //取得使用者輸入的選項，並轉成數值，以便於switch-case的比對
+                int choice = Integer.parseInt(((EditText)v).getText().toString());
+
+                if(((EditText)v)==et3){
+                    //當使用者互動的是EditText3時，根據使用者的選擇，來顯示特定文字
+                    switch(choice){
+                        case 1:Toast.makeText(CustomToastQuest.this, R.string.choice1, Toast.LENGTH_SHORT).show(); break;
+                        case 2:Toast.makeText(CustomToastQuest.this, R.string.choice2, Toast.LENGTH_SHORT).show(); break;
+                        case 3:Toast.makeText(CustomToastQuest.this, R.string.choice3, Toast.LENGTH_SHORT).show(); break;
+                        case 4:Toast.makeText(CustomToastQuest.this, R.string.choice4, Toast.LENGTH_SHORT).show(); break;
+                        default:Toast.makeText(CustomToastQuest.this, R.string.choice5, Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else
+                    //當使用者互動的是EditText1與EditText2時，就顯示使用者所輸入的資料
+                    Toast.makeText(CustomToastQuest.this, ((EditText)v).getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+            // TODO Auto-generated method stub
+            return false;
+        }
+    };
+}
